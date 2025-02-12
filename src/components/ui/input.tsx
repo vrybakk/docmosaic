b94463e254f93971a01c9ce07148d51b47187c57
Vladyslav Rybak <rybak.vlad00@gmail.com>
2025-02-12 20:04:09 +0100

    fix(ui): update input component type definition

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,7 +1,7 @@
 import { cn } from '@/lib/utils';
 import { forwardRef } from 'react';
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 const Input = forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
   return (
