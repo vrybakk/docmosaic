005eba9d30a1159e14dc01af71398af5f2dcfb3d
Vladyslav Rybak <rybak.vlad00@gmail.com>
2026-06-06 18:53:21 +0200

    fix: open Buy Me a Coffee button via anchor link

--- src/components/donate-button.tsx
+++ src/components/donate-button.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button } from '@/components/ui/core/button';
+import { CustomLink } from '@/components/ui/core/link';
 import { cn } from '@/lib/utils';
 import { Coffee } from 'lucide-react';
 
@@ -16,14 +16,15 @@ export default function DonateButton({
     className,
 }: DonateButtonProps) {
     return (
-        <Button
+        <CustomLink
             variant={variant === 'coffee' ? 'caramel' : variant}
             size={size}
-            onClick={() => window.open('https://buymeacoffee.com/vrybakk', '_blank')}
-            icon={<Coffee className={`w-4 h-4`} />}
+            href="https://buymeacoffee.com/vrybakk"
+            external
+            icon={<Coffee className="w-4 h-4" />}
             className={cn('w-fit buy-me-a-coffee-click-trigger', className)}
         >
             Buy Me a Coffee
-        </Button>
+        </CustomLink>
     );
 }
